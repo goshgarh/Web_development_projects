5be441a0b105a4595fb4b3bbf6e1659422a4f1c8
goshgarh <goshgar@yahoo.com>
2018-03-04 09:24:48 -0500

    Add files via upload

---
+++ Responsive_prj/js/app.js
@@ -0,0 +1,11 @@
+var a = document.getElementsByTagName('a');
+
+document.addEventListener('click', function(event) { 
+ if (event.target.tagName='a') {
+     a.style.backgroundColor=
+ }
+})
+
+
+
+background:#efeadd;
